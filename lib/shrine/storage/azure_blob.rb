26f574f32e127ff62ca92e58f1cffd58861896bb
neongrau <neongrau@gmail.com>
2020-06-17 18:13:50 +0200

    cleanup
    some more DRY to re-use the client

--- lib/shrine/storage/azure_blob.rb
+++ lib/shrine/storage/azure_blob.rb
@@ -13,16 +13,16 @@ class Shrine
     class AzureBlob
       attr_reader :client, :account_name, :access_key, :container_name, :multipart_threshold, :public
 
-      def initialize(
-        account_name: nil, access_key: nil,
-        container_name: nil, multipart_threshold: {},
-        public: nil
-      )
+      def initialize(account_name: nil, access_key: nil, container_name: nil, multipart_threshold: {}, public: nil)
         @access_key = access_key
         @account_name = account_name
         @container_name = container_name
         @multipart_threshold = multipart_threshold
         @sas = Azure::Storage::Common::Core::Auth::SharedAccessSignature.new account_name, access_key
+        @client = Azure::Storage::Blob::BlobService.create(
+          storage_account_name: account_name,
+          storage_access_key: access_key
+        )
         @public = public
       end
 
@@ -47,48 +47,29 @@ class Shrine
 
       def open(id, _rewindable: false, **_options)
         GC.start
-
-        client = Azure::Storage::Blob::BlobService.create(
-          storage_account_name: account_name,
-          storage_access_key: access_key
-        )
-
-        _blob, content = client.get_blob(container_name, id)
+        _blob, content = @client.get_blob(container_name, id)
         StringIO.new(content)
       end
 
       def put(io, id, **_options)
-        client = Azure::Storage::Blob::BlobService.create(
-          storage_account_name: account_name,
-          storage_access_key: access_key
-        )
         if (path = extract_path(io))
           ::File.open(path, 'rb') do |file|
-            client.create_block_blob(container_name, id, file.read, timeout: 30)
+            @client.create_block_blob(container_name, id, file.read, timeout: 30, **_options)
           end
         else
-          client.create_block_blob(container_name, id, io.to_io)
+          @client.create_block_blob(container_name, id, io.to_io, **_options)
         end
       end
 
       def delete(id)
-        client = Azure::Storage::Blob::BlobService.create(
-          storage_account_name: account_name,
-          storage_access_key: access_key
-        )
-        client.delete_blob(container_name, id)
+        @client.delete_blob(container_name, id)
       end
-
 
       # :public
       # :  Returns the unsigned URL to the S3 object. This requires the S3
       #    object to be public.
       def url(id, public: self.public, **options)
-        client = Azure::Storage::Blob::BlobService.create(
-          storage_account_name: account_name,
-          storage_access_key: access_key
-        )
-        uri = client.generate_uri("#{container_name}/#{id}")
+        uri = @client.generate_uri("#{container_name}/#{id}")
         uri.scheme = options[:scheme] || 'https'
         unless public
           uri.query = @sas.generate_service_sas_token(
